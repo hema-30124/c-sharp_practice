3b817bf47dd8255bb0466db27c293d8233a37c0a
hema-30124 <your.email@example.com>
2024-12-20 12:02:25 +0530

    Updated Program script with movement logic

--- Program.cs
+++ Program.cs
@@ -1,5 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
+using UnityEngine;
 
+public class Program : MonoBehaviour
+{
+    public float speed = 5f;
+    void update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(horizontal, 0, 0) * speed * Time.deltaTime;
+        transform.Translate(movement);
+    }
+}
 
